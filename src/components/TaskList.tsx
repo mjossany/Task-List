03e04f005b6699f926e952aeadbd333ac3bfa1ce
Jossany Moura <mjossany@gmail.com>
2022-05-10 16:46:22 -0300

    Implement handleToggleTaskCompletion

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -22,7 +22,13 @@ export function TaskList() {
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
-    
+    const newArray = tasks.map(task => {
+      if (task.id === id) {
+        return {...task, isComplete: !task.isComplete}
+      }
+      return task;
+    });
+    setTasks(newArray);
   }
 
   function handleRemoveTask(id: number) {
