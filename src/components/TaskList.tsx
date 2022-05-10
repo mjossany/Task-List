aedb12b9549c10c6e10404847dd3d295173ae184
Jossany Moura <mjossany@gmail.com>
2022-05-10 16:19:37 -0300

    Implement handleCreateNewTask function

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,6 +16,8 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    newTaskTitle && setTasks([...tasks, { id: Math.floor(Math.random() * 100) + 1, title: newTaskTitle, isComplete: false}])
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
